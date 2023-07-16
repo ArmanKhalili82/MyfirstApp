927e978258b6db3c5d63383f029b0ecf08216ae9
ArmanKhalili82 <hykhalili@yahoo.com>
2023-07-16 03:05:47 +0200

    Testing not gonna work

---
+++ src/components/footer/ChangePicture.test.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import ChangePicture from "./ChangePicture"
+
+
+
+test('renders changed! i want to know when button clicked', () => {
+    render(<ChangePicture />);
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+})
