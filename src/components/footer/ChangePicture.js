0f432bffc32bb96e5fd8abacdf4dfd1967bafc0a
alikh <alikh@outlook.com>
2023-07-16 17:09:03 +0200

    fixed tests

--- src/components/footer/ChangePicture.js
+++ src/components/footer/ChangePicture.js
@@ -1,32 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './ChangePicture.css'
 import Image1 from '../../images/IMG_8.jpg'
 import Image2 from '../../images/IMG_7.jpg'
 
-const Pictures = [ Image1, Image2 ]
-    
+const Pictures = [Image1, Image2]
+
 
 const ChangePicture = () => {
-    const [picture, setPicture] = useState(Pictures[0]);
+    const [picture, setPicture] = useState(Image1);
 
     const ChangeHandler = () => {
         if (picture === Image1) {
             setPicture(Image2)
         }
-
         else {
             setPicture(Image1)
         }
-    } 
+    }
 
-  return (
-    <div className='Full'>
-        <div id={ picture == Image1? "I1": "I2"} className='change-picture'>
-            <img src={picture} />
-            <button  onClick={ChangeHandler} className='change-btn'>Change Picture</button>
+    return (
+        <div className='Full'>
+            <div data-testid={picture === Image1 ? "I1" : "I2"} className='change-picture'>
+                <img src={picture} />
+                <button onClick={ChangeHandler} className='change-btn'>Change Picture</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ChangePicture
--- src/components/footer/ChangePicture.test.js
+++ src/components/footer/ChangePicture.test.js
@@ -1,14 +1,23 @@
-import { render } from "@testing-library/react"
+import React from 'react'
+import { render, screen, act, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import ChangePicture from "./ChangePicture"
 
 
 
-test('renders changed! i want to know when button clicked', () => {
-    render(<ChangePicture />);
+test('renders changed! i want to know when button clicked', async () => {
+    const { getByText } = render(<ChangePicture />);
 
-    const buttonElement = screen.getByRole('button');
+    await waitFor(() => {
+        const i1 = screen.getByTestId('I1')
+        expect(i1).not.toBeNull();
+    })
+
+    const buttonElement = getByText('Change Picture');
     userEvent.click(buttonElement)
-    const I2 = document.getElementById('I2')
-    expect(I2).not.toBeNull(I2);
+
+    await waitFor(() => {
+        const i2 = screen.getByTestId('I2')
+        expect(i2).not.toBeNull();
+    })
 })
