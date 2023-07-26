f4e202c658191a8f8147909dc398a8758a8741a5
ArmanKhalili82 <hykhalili@yahoo.com>
2023-07-26 17:43:53 +0200

    Access

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.writeHead(200, {'Content-Type': 'application/json'});
     let response = {'name': 'Arman'}
     res.end(JSON.stringify(response));
