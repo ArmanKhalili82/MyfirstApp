d21b64498c6c89ad0773cedaf789b2de5d0478fd
ArmanKhalili82 <hykhalili@yahoo.com>
2023-07-29 15:41:14 +0200

    info

--- src/components/footer/ChangePicture.js
+++ src/components/footer/ChangePicture.js
@@ -9,6 +9,7 @@ const Pictures = [Image1, Image2]
 
 const ChangePicture = () => {
     const [picture, setPicture] = useState(Image1);
+    const [information, setInformation] = useState('');
 
     const ChangeHandler = async() => {
 
@@ -26,11 +27,23 @@ const ChangePicture = () => {
         }
     }
 
+
+    const InformationHandler = async() => {
+        const response = await fetch("http://localhost:8080");
+        const info = await response.json();
+        console.log(info);
+        setInformation(info);
+    }
+
     return (
         <div className='Full'>
             <div data-testid={picture === Image1 ? "I1" : "I2"} className='change-picture'>
                 <img src={picture} />
                 <button onClick={ChangeHandler} className='change-btn'>Change Picture</button>
+            </div>
+            <div>
+                <button onClick={InformationHandler}>Show Me</button>
+                <div className={information}></div>
             </div>
         </div>
     )
